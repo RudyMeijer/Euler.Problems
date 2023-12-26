c0059c49cd59b049acd5ebccb04de7d8dd540724
Rudy Meijer <rudy.meijer@gmail.com>
2023-12-26 15:13:03 +0100

    Code cleanup

--- Problems50/Problem66.cs
+++ Problems50/Problem66.cs
@@ -1,114 +1,110 @@
-﻿using System.Drawing;
-using System;
-using System.Collections.Generic;
-
-namespace Euler.Solutions
+﻿namespace Euler.Solutions
 {
-	/// <summary>
-	/// see http://projecteuler.net/index.php?section=problems&id=66
-	///
-	/// analyse: solve x2 – Dy2 = 1
-	/// 
-	/// performance improvements: use Chakravala method http://en.wikipedia.org/wiki/Chakravala_method.
-	///</summary>
-	class Problem66 : Helper, IProblem
-	{
-		public double Execute()
-		{
-			var sum = 0.0;
-			var list = new long[1001];
-			for (int D = 1; D <= 1000; D++)
-			{
-                Console.WriteLine($"D={D}");
-				var sqr = Math.Sqrt(D);
-				if (sqr == (int)sqr) { list[D] = -1; continue; } // D is kwadratic.
-				if (list[D] == 0)
-				{
+    /// <summary>
+    /// see http://projecteuler.net/index.php?section=problems&id=66
+    ///
+    /// analyse: solve x² – Dy² = 1
+    /// 
+    /// performance improvements: use Chakravala method http://en.wikipedia.org/wiki/Chakravala_method.
+    ///</summary>
+    class Problem66 : Helper, IProblem
+    {
+        public double Execute()
+        {
+            var sum = 0.0;
+            var list = new long[1001];
+            for (int D = 1; D <= 1000; D++)
+            {
+                //Console.WriteLine($"D={D}");
+                var sqr = Math.Sqrt(D);
+                if (sqr == (int)sqr) { list[D] = -1; continue; } // D is kwadratic.
+                if (list[D] == 0)
+                {
 
-					var m = new Triple(D);
-					while (Math.Abs(m.k) != 1)
-					{
-						m = m.M();
-					}
-					// solution found;
-					list[D] = m.a;
-					sum = 1.0 * m.a / m.b;
-				}
-			}
-			return sum;
-		}
+                    Triple m = new(D);
+                    while (m.k != 1)
+                    {
+                        m = m.M();
+                    }
+                    // solution found;
+                    list[D] = m.a;
+                    Console.WriteLine($"m= {m}");
+                    sum = 1.0 * m.a / m.b;
+                }
+            }
+            return sum;
+        }
 
 
-	}
-	/// <summary>
-	/// x.x-N.y.y = k
-	/// </summary>
-	class Triple
-	{
-		public long a;
-		public long b;
-		public long k;
-		public long N;
-		public Triple(int N)
-		{
-			a = (int)(Math.Sqrt(N) + 0.5);
-			b = 1;
-			k = a * a - N;
-			this.N = N;
-		}
-		public Triple(long a, long b, long k)
-		{
-			this.a = a;
-			this.b = b;
-			this.k = k;
-			this.N = (a * a - k) / (b * b);
-		}
-		public Triple M()
-		{
-			//
-			// bepaal m: (a+m) deelbaar door k en (m.m - N)/k is minimaal.
-			//
-			var m = (int)(Math.Sqrt(N) - 1);
-			var remainder1 = 1L;
-			var remainder2 = 1L;
-			while (remainder1 != 0 || remainder2 != 0)
-			{
-				++m;
-				Math.DivRem(m * m - N, k, out remainder1);
-				Math.DivRem(a + b * m, k, out remainder2);
-			}
-			var k_ = Math.Abs(k);
-			var a1 = (a * m + N * b) / k_;
-			var b1 = (a + b * m) / k_;
-			var k1 = (m * m - N) / k;
-			if (Math.Abs(k1) == 2)
-			{
-				var u = a1;
-				a1 = u * u + ((k1==2)?-1:+1);
-				b1 = u * b1;
-				k1 = 1;
-			}
-			if (Math.Abs(k1) == 4)
-			{
-				var u = a1;
-				a1 = (u * u * u * u + 4 * u * u + 1) * (u * u + 2) / 2;
-				b1 = (u * u + 3) * (u * u + 1) * u * b1 / 2;
-				k1 = 1;
-			}
-			return new Triple(a1, b1, k1);
-		}
-		public Triple Compose(Triple t)
-		{
-			var a1 = (a * t.a + N * b * t.b);
-			var b1 = (a * t.b + t.a * b);
-			var k1 = k * t.k;
+    }
+    /// <summary>
+    /// a²-N.b² = k
+    /// </summary>
+    class Triple
+    {
+        public long a,b,k,N;
+        public Triple(int N)
+        {
+            a = (int)(Math.Sqrt(N) + 0.5);
+            b = 1;
+            k = a * a - N;
+            this.N = N;
+        }
+        public Triple(long a, long b, long k)
+        {
+            this.a = a;
+            this.b = b;
+            this.k = k;
+            this.N = (a * a - k) / (b * b);
+        }
+        /// <summary>
+        /// 
+        /// bepaal m: (a+m) deelbaar door k en (m² - N)/k is minimaal.
+        /// 
+        /// </summary>
+        public Triple M()
+        {
+            var m = (int)(Math.Sqrt(N) - 1);
+            var remainder1 = 1L;
+            var remainder2 = 1L;
+            while (remainder1 != 0 || remainder2 != 0)
+            {
+                ++m;
+                Math.DivRem(m * m - N, k, out remainder1);
+                Math.DivRem(a + b * m, k, out remainder2);
+            }
+            var k_ = Math.Abs(k);
+            var a1 = (a * m + N * b) / k_;
+            var b1 = (a + b * m) / k_;
+            var k1 = (m * m - N) / k;
+            if (Math.Abs(k1) == 2)
+            {
+                var u = a1;
+                a1 = u * u + ((k1 == 2) ? -1 : +1);
+                b1 = u * b1;
+                k1 = 1;
+            }
+            if (Math.Abs(k1) == 4)
+            {
+                var u = a1;
+                a1 = (u * u * u * u + 4 * u * u + 1) * (u * u + 2) / 2;
+                b1 = (u * u + 3) * (u * u + 1) * u * b1 / 2;
+                k1 = 1;
+            }
+            return new Triple(a1, b1, k1);
+        }
+        public Triple Compose(Triple t)
+        {
+            var a1 = (a * t.a + N * b * t.b);
+            var b1 = (a * t.b + t.a * b);
+            var k1 = k * t.k;
             return new Triple(a1 / k, b1 / k, k1 / k);
-		}
-		public override string ToString()
-		{
-			return String.Format("{0}, {1}, {2}, N={3}", a, b, k, N);
-		}
-	}
+        }
+        public override string ToString()
+        {
+            return String.Format($"{a,5}, {b,5}, N={N}");
+        }
+    }
 }
 
 //private void make(dynamic triple, Tuple<int, int, int> triple1)
--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Reflection;
 
 namespace Euler.Solutions
 {
@@ -29,93 +25,94 @@ namespace Euler.Solutions
             /// Find all problem classes in this assembly.
             /// 
             var Problems = from Problem in Assembly.GetExecutingAssembly().GetTypes()
-                           where Problem.Name.StartsWith("Problem") // Remove number to execute all tests.
-                           orderby  Problem.Name ascending select Problem;
+                           where Problem.Name.StartsWith("Problem66") // Remove number to execute all tests.
+                           orderby Problem.Name ascending
+                           select Problem;
             ///
             /// Execute each problem and show results.
             ///
             foreach (var Problem in Problems)
             {
                 IProblem problem = (IProblem)Activator.CreateInstance(Problem);
-                UnitTest.Pass(problem,1);
-                Console.WriteLine("{0,-9} ={1,12} elapsed time:{2,5} ms. Test {3}.", Problem.Name, UnitTest.result, UnitTest.ElapsedMilliseconds, UnitTest.passed?"Passed":"Failed !!!");
+                UnitTest.Pass(problem, 1);
+                Console.WriteLine("{0,-9} ={1,12} elapsed time:{2,5} ms. Test {3}.", Problem.Name, UnitTest.result, UnitTest.ElapsedMilliseconds, UnitTest.passed ? "Passed" : "Failed !!!");
                 //break; // only execute last problem
             }
-            Console.WriteLine("Total Time: {0} ms",UnitTest.TotalTime);
-			//Helper : 2 ms.                      .net 4.0
-			//Problem00 =          -1 elapsed time:    4 ms. Test Passed.    3 ms.
-			//Problem01 =      233168 elapsed time:    1 ms. Test Passed.    1 ms.
-			//Problem02 =     4613732 elapsed time:    6 ms. Test Passed.    1 ms.
-			//Problem03 =        6857 elapsed time:  111 ms. Test Passed.   58 ms.
-			//Problem04 =      906609 elapsed time:    2 ms. Test Passed.    1 ms.
-			//Problem05 =   232792560 elapsed time:   11 ms. Test Passed.  102 ms.
-			//Problem06 =    25164150 elapsed time:    0 ms. Test Passed.    2 ms.
-			//Problem07 =      104743 elapsed time:    0 ms. Test Passed.    8 ms.
-			//Problem08 =       40824 elapsed time:    0 ms. Test Passed.    0 ms.
-			//Problem09 =    31875000 elapsed time:    1 ms. Test Passed.    0 ms.
-			//Problem10 =142913828922 elapsed time:  144 ms. Test Passed.   70 ms.
-			//Problem11 =    70600674 elapsed time:    1 ms. Test Passed.    0 ms.
-			//Problem12 =    76576500 elapsed time:   81 ms. Test Passed.   56 ms.
-			//Problem13 =  5537376230 elapsed time:    3 ms. Test Passed.    2 ms.
-			//Problem14 =      837799 elapsed time:   81 ms. Test Passed.  139 ms.
-			//Problem15 =137846528820 elapsed time:    0 ms. Test Passed.    0 ms.
-			//Problem16 =        1366 elapsed time:    8 ms. Test Passed.    4 ms.
-			//Problem17 =       21124 elapsed time:    5 ms. Test Passed.    4 ms.
-			//Problem18 =        1074 elapsed time:    1 ms. Test Passed.    0 ms.
-			//Problem19 =         171 elapsed time:    1 ms. Test Passed.    1 ms.
-			//Problem20 =         648 elapsed time:    9 ms. Test Passed.    3 ms.
-			//Problem21 =       31626 elapsed time:    8 ms. Test Passed.    4 ms.
-			//Problem22 =   871198282 elapsed time:   32 ms. Test Passed.   23 ms.
-			//Problem23 =     4179871 elapsed time:  191 ms. Test Passed.  123 ms.
-			//Problem24 =  2783915460 elapsed time:    1 ms. Test Passed.   29 ms.
-			//Problem25 =        4782 elapsed time:    0 ms. Test Passed.
-			//Problem26 =         983 elapsed time:    2 ms. Test Passed.
-			//Problem27 =      -59231 elapsed time:   11 ms. Test Passed.
-			//Problem28 =   669171001 elapsed time:    0 ms. Test Passed.
-			//Problem29 =        9183 elapsed time:   25 ms. Test Passed.
-			//Problem30 =      443839 elapsed time:   27 ms. Test Passed.
-			//Problem31 =       73682 elapsed time:    3 ms. Test Passed.
-			//Problem32 =       45228 elapsed time:   11 ms. Test Passed.
-			//Problem33 =         100 elapsed time:    0 ms. Test Passed.
-			//Problem34 =       40730 elapsed time:    5 ms. Test Passed.
-			//Problem35 =          55 elapsed time:   11 ms. Test Passed.
-			//Problem36 =      872187 elapsed time:    4 ms. Test Passed.
-			//Problem37 =      748317 elapsed time:   16 ms. Test Passed.
-			//Problem38 =   932718654 elapsed time:    1 ms. Test Passed.
-			//Problem39 =         840 elapsed time:    1 ms. Test Passed.
-			//Problem40 =         210 elapsed time:    1 ms. Test Passed.
-			//Problem41 =     7652413 elapsed time:    2 ms. Test Passed.
-			//Problem42 =         162 elapsed time:   23 ms. Test Passed.
-			//Problem43 = 16695334890 elapsed time:   20 ms. Test Passed.
-			//Problem44 =     5482660 elapsed time:    4 ms. Test Passed.
-			//Problem45 =  1533776805 elapsed time:    3 ms. Test Passed.
-			//Problem46 =        5777 elapsed time:   19 ms. Test Passed.
-			//Problem47 =      134043 elapsed time:  165 ms. Test Passed.
-			//Problem48 =  9110846700 elapsed time:   19 ms. Test Passed.
-			//Problem49 =296962999629 elapsed time:    5 ms. Test Passed.
-			//Problem50 =      997651 elapsed time:    1 ms. Test Passed.
-			//Problem51 =      121313 elapsed time:   19 ms. Test Passed.
-			//Problem52 =      142857 elapsed time:   11 ms. Test Passed.
-			//Problem53 =        4075 elapsed time:    0 ms. Test Passed.
-			//Problem54 =         376 elapsed time:   16 ms. Test Passed.
-			//Problem55 =         249 elapsed time:    5 ms. Test Passed.
-			//Problem56 =         972 elapsed time:   24 ms. Test Passed.
-			//Problem57 =         153 elapsed time:    1 ms. Test Passed.
-			//Problem58 =       26241 elapsed time:  239 ms. Test Passed.
-			//Problem59 =      107359 elapsed time:   59 ms. Test Passed.
-			//Problem60 =       26033 elapsed time:  421 ms. Test Passed.
-			//Problem61 =       28684 elapsed time:    3 ms. Test Passed.
-			//Problem62 =127035954683 elapsed time:   48 ms. Test Passed.
-			//Problem63 =          49 elapsed time:    0 ms. Test Passed.
-			//Problem64 =        1322 elapsed time:   16 ms. Test Passed.
-			//Problem65 =         272 elapsed time:    3 ms. Test Passed.
-			//Total Time: 1946 ms
-			//Press any key to continue . . .
+            Console.WriteLine("Total Time: {0} ms", UnitTest.TotalTime);
+            //Helper : 2 ms.                      .net 4.0
+            //Problem00 =          -1 elapsed time:    4 ms. Test Passed.    3 ms.
+            //Problem01 =      233168 elapsed time:    1 ms. Test Passed.    1 ms.
+            //Problem02 =     4613732 elapsed time:    6 ms. Test Passed.    1 ms.
+            //Problem03 =        6857 elapsed time:  111 ms. Test Passed.   58 ms.
+            //Problem04 =      906609 elapsed time:    2 ms. Test Passed.    1 ms.
+            //Problem05 =   232792560 elapsed time:   11 ms. Test Passed.  102 ms.
+            //Problem06 =    25164150 elapsed time:    0 ms. Test Passed.    2 ms.
+            //Problem07 =      104743 elapsed time:    0 ms. Test Passed.    8 ms.
+            //Problem08 =       40824 elapsed time:    0 ms. Test Passed.    0 ms.
+            //Problem09 =    31875000 elapsed time:    1 ms. Test Passed.    0 ms.
+            //Problem10 =142913828922 elapsed time:  144 ms. Test Passed.   70 ms.
+            //Problem11 =    70600674 elapsed time:    1 ms. Test Passed.    0 ms.
+            //Problem12 =    76576500 elapsed time:   81 ms. Test Passed.   56 ms.
+            //Problem13 =  5537376230 elapsed time:    3 ms. Test Passed.    2 ms.
+            //Problem14 =      837799 elapsed time:   81 ms. Test Passed.  139 ms.
+            //Problem15 =137846528820 elapsed time:    0 ms. Test Passed.    0 ms.
+            //Problem16 =        1366 elapsed time:    8 ms. Test Passed.    4 ms.
+            //Problem17 =       21124 elapsed time:    5 ms. Test Passed.    4 ms.
+            //Problem18 =        1074 elapsed time:    1 ms. Test Passed.    0 ms.
+            //Problem19 =         171 elapsed time:    1 ms. Test Passed.    1 ms.
+            //Problem20 =         648 elapsed time:    9 ms. Test Passed.    3 ms.
+            //Problem21 =       31626 elapsed time:    8 ms. Test Passed.    4 ms.
+            //Problem22 =   871198282 elapsed time:   32 ms. Test Passed.   23 ms.
+            //Problem23 =     4179871 elapsed time:  191 ms. Test Passed.  123 ms.
+            //Problem24 =  2783915460 elapsed time:    1 ms. Test Passed.   29 ms.
+            //Problem25 =        4782 elapsed time:    0 ms. Test Passed.
+            //Problem26 =         983 elapsed time:    2 ms. Test Passed.
+            //Problem27 =      -59231 elapsed time:   11 ms. Test Passed.
+            //Problem28 =   669171001 elapsed time:    0 ms. Test Passed.
+            //Problem29 =        9183 elapsed time:   25 ms. Test Passed.
+            //Problem30 =      443839 elapsed time:   27 ms. Test Passed.
+            //Problem31 =       73682 elapsed time:    3 ms. Test Passed.
+            //Problem32 =       45228 elapsed time:   11 ms. Test Passed.
+            //Problem33 =         100 elapsed time:    0 ms. Test Passed.
+            //Problem34 =       40730 elapsed time:    5 ms. Test Passed.
+            //Problem35 =          55 elapsed time:   11 ms. Test Passed.
+            //Problem36 =      872187 elapsed time:    4 ms. Test Passed.
+            //Problem37 =      748317 elapsed time:   16 ms. Test Passed.
+            //Problem38 =   932718654 elapsed time:    1 ms. Test Passed.
+            //Problem39 =         840 elapsed time:    1 ms. Test Passed.
+            //Problem40 =         210 elapsed time:    1 ms. Test Passed.
+            //Problem41 =     7652413 elapsed time:    2 ms. Test Passed.
+            //Problem42 =         162 elapsed time:   23 ms. Test Passed.
+            //Problem43 = 16695334890 elapsed time:   20 ms. Test Passed.
+            //Problem44 =     5482660 elapsed time:    4 ms. Test Passed.
+            //Problem45 =  1533776805 elapsed time:    3 ms. Test Passed.
+            //Problem46 =        5777 elapsed time:   19 ms. Test Passed.
+            //Problem47 =      134043 elapsed time:  165 ms. Test Passed.
+            //Problem48 =  9110846700 elapsed time:   19 ms. Test Passed.
+            //Problem49 =296962999629 elapsed time:    5 ms. Test Passed.
+            //Problem50 =      997651 elapsed time:    1 ms. Test Passed.
+            //Problem51 =      121313 elapsed time:   19 ms. Test Passed.
+            //Problem52 =      142857 elapsed time:   11 ms. Test Passed.
+            //Problem53 =        4075 elapsed time:    0 ms. Test Passed.
+            //Problem54 =         376 elapsed time:   16 ms. Test Passed.
+            //Problem55 =         249 elapsed time:    5 ms. Test Passed.
+            //Problem56 =         972 elapsed time:   24 ms. Test Passed.
+            //Problem57 =         153 elapsed time:    1 ms. Test Passed.
+            //Problem58 =       26241 elapsed time:  239 ms. Test Passed.
+            //Problem59 =      107359 elapsed time:   59 ms. Test Passed.
+            //Problem60 =       26033 elapsed time:  421 ms. Test Passed.
+            //Problem61 =       28684 elapsed time:    3 ms. Test Passed.
+            //Problem62 =127035954683 elapsed time:   48 ms. Test Passed.
+            //Problem63 =          49 elapsed time:    0 ms. Test Passed.
+            //Problem64 =        1322 elapsed time:   16 ms. Test Passed.
+            //Problem65 =         272 elapsed time:    3 ms. Test Passed.
+            //Total Time: 1946 ms
+            //Press any key to continue . . .
         }
         public static void ConsoleWriteToFile(string outputFile)
         {
             StreamWriter sw = new(outputFile);
-            sw.AutoFlush=true;
+            sw.AutoFlush = true;
             Console.SetOut(sw);
         }
     }
